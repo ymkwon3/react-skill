54905fc18f312718d1f9cbfb41a094deeac4f5dc
ingmin <ymkwon3@gmail.com>
2022-03-28 23:25:58 +0900

    무한스크롤 제외 완성(한것같음)

--- src/redux/modules/wordModule.js
+++ src/redux/modules/wordModule.js
@@ -1,65 +1,81 @@
 import { db } from "../../firebase";
-import { doc, getDoc, updateDoc, arrayUnion, setDoc } from "firebase/firestore";
+import { doc, getDocs, updateDoc, deleteField, collection, setDoc, deleteDoc } from "firebase/firestore";
+
+const words_doc = doc(db, 'words', 'words');
+const words_collection = collection(db, "words");
 
 // Actions
 const LOAD = "wordModules/LOAD";
-const ADD = "wordModules/ADD";
-const CHECK = "wordModules/CHECK";
+const DELETE = "wordModules/DELETE";
 
 const initialState = {
-  is_loaded: false,
-  words: {},
+  words: [],
   progress: 0
 };
 
 // Action Creators
-
 export function loadWord(word_list) {
   return { type: LOAD, word_list };
 }
 
+export function deleteWord(term) {
+  return { type: DELETE, term };
+}
+
+
 // middlewares
 export const loadWordFB = () => {
   return async function (dispatch) {
-    const data = await getDoc(doc(db, "words", "words"));
-    dispatch(loadWord(data.data()));
+    const data = await getDocs(words_collection);
+    let list = [];
+    data.forEach((v) => {
+      list.push({...v.data()});
+    });
+    dispatch(loadWord(list));
   };
 };
 
-export const addWordFB = (term, mean, link, check) => {
+export const addWordFB = (obj) => {
   return async function () {
-    await updateDoc(doc(db, "words", "words"), {
-      [term]: [term, mean, link, check]
-    });
-    // updateDoc(doc(db, "words", "words"), {
-    //   words: arrayUnion({
-    //     term: term,
-    //     mean: mean,
-    //     link: link,
-    //   }),
-    // });
+    await setDoc(doc(db, "words", obj.term), obj);
   };
 };
 
-export const checkWordFB = (term, mean, link, check) => {
+export const deleteWordFB = (term) => {
   return async function (dispatch) {
-    await updateDoc(doc(db, "words", "words"), {
-      [term]: [term, mean, link, check]
-    });
-    dispatch(loadWordFB());
+    const docRef = doc(db, "words", term);
+    await deleteDoc(docRef);
+    alert("삭제했다람쥐...")
+    dispatch(deleteWord(term));
   };
 };
+
+export const checkWordFB = (obj) => {
+  return async function (dispatch, getState) {
+    const docRef = doc(db, "words", obj.term);
+    await updateDoc(docRef, obj);
+    const word_list = getState().wordModule.words.map(v => v.term === obj.term ? obj : v);
+    console.log(word_list)
+    dispatch(loadWord(word_list));
+  };
+};
+
 
 // Reducer
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case "wordModules/LOAD": {
-      const word_list = Object.entries(action.word_list).sort();
-      const progressCnt = word_list.reduce((acc, cur) => {
-        return cur[1][3] ? acc + 1 : acc + 0;
-      }, 0)
-      let map = new Map(word_list);
-      return { ...state, words: map, progress: progressCnt / word_list.length};
+      const word_list = action.word_list.sort();
+
+      // 데이터베이스에 단어가 없으면 progress를 0으로, 단어가 존재하면 계산
+      const progress = word_list.length != 0 ? word_list.reduce((acc, cur) => {
+        return cur.checked ? acc + 1 : acc + 0;
+      }, 0) / word_list.length : 0;
+      return { ...state, progress, words: word_list};
+    }
+    case "wordModules/DELETE": {
+      const words = state.words.filter(v => v.term != action.term);
+      return {...state, words: words}
     }
     default:
       return state;
